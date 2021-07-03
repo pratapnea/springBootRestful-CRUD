ba6f1eb8b4d96a22674ed7d053812c19657757eb
pratapnea <sereversion@gmail.com>
2021-07-03 11:21:38 +0545

    initial spring boot

---
+++ src/main/java/com/example/springboot/SpringbootCrudRestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservicesApplication.class, args);
+	}
+
+}
