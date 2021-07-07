edda936b8c31a64ece19b6fcb64a04ea56c0ae8b
pratapnea <sereversion@gmail.com>
2021-07-07 12:18:12 +0545

    files updated and ready to go with swagger

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -20,49 +20,47 @@ import com.example.springboot.repository.UserRepository;
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	//	get all users 
-	@GetMapping
-	public List<User> getAllUsers() {
-		return this.userRepository.findAll();
-	}
-	
-	//	get users by id
-	@GetMapping("/{id}")
-	public User getuserById(@PathVariable (value = "id") long userId) {
-		return this.userRepository.findById(userId)
-				.orElseThrow(() -> new ResourceNotFoundException("User not found with id:" + userId)) ;
-	}
-	
-	//	create user
-	@PostMapping
-	public User createUser(@RequestBody User user) {
-		return this.userRepository.save(user);
-	}
-	
-	//	update user
-	@PutMapping("/{id}")
-	public User updateUser(@RequestBody User user, @PathVariable ("id") long userId) {
-		User existingUser = this.userRepository.findById(userId)
-				.orElseThrow(() -> new ResourceNotFoundException("User not found with id:" + userId)) ;
-		existingUser.setFirstName(user.getFirstName());
-		existingUser.setLastName(user.getLastName());
-		existingUser.setEmail(user.getEmail());
-		
-		return this.userRepository.save(existingUser);
-	}
-	
-	//	delete user by id
-	@DeleteMapping("/{id}")
-	public ResponseEntity<User> deleteUser(@PathVariable ("id") long userId) {
-		User existingUser = this.userRepository.findById(userId)
-				.orElseThrow(() -> new ResourceNotFoundException("User not found with id:" + userId)) ;
-		this.userRepository.delete(existingUser);
-		return ResponseEntity.ok().build();
-	}
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //	get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    //	get users by id
+    @GetMapping("/{id}")
+    public User getuserById(@PathVariable(value = "id") long userId) {
+        return this.userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id:" + userId));
+    }
+
+    //	create user
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return this.userRepository.save(user);
+    }
+
+    //	update user
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable("id") long userId) {
+        User existingUser = this.getuserById(userId);
+        existingUser.setFirstName(user.getFirstName());
+        existingUser.setLastName(user.getLastName());
+        existingUser.setEmail(user.getEmail());
+
+        return this.userRepository.save(existingUser);
+    }
+
+    //	delete user by id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") long userId) {
+        User existingUser = this.getuserById(userId);
+        this.userRepository.delete(existingUser);
+        return ResponseEntity.ok().build();
+    }
 }
 
 
--- src/main/java/com/example/springboot/entity/User.java
+++ src/main/java/com/example/springboot/entity/User.java
@@ -8,57 +8,64 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
-	
-	@Column(name = "first_name")
-	private String firstName;
-	
-	@Column(name = "last_name")
-	private String lastName;
-	
-	@Column(name = "email")
-	private String email;
-	
-	public User() {
-		
-	}
-	
-	public User(String firstName, String lastName, String email) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
 }
